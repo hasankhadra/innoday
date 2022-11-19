62c87e4f77f7f772a76810885cd9682e82dd8d0d
hasankhadra <59062695+hasankhadra@users.noreply.github.com>
2022-11-19 14:02:33 +0300

    fix: env vars

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,14 +1,27 @@
+// const firebaseConfig = {
+//     apiKey: process.env.API_KEY,
+//     authDomain: process.env.AUTH_DOMAIN,
+//     projectId: process.env.PROJECT_ID,
+//     storageBucket: process.env.STORAGE_BUCKET,
+//     messagingSenderId: process.env.MESSAGING_SENDER_ID,
+//     appId: process.env.APP_ID,
+//     measurementId: process.env.MEASUREMENT_ID,
+// }
+
 const firebaseConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID,
-    measurementId: process.env.MEASUREMENT_ID,
+    apiKey: 'AIzaSyAc2Z8DBuSWL_ifUmRFH5ukChjUlWeW3e8',
+    authDomain: 'innoday-6b9ff.firebaseapp.com',
+    projectId: 'innoday-6b9ff',
+    storageBucket: 'innoday-6b9ff.appspot.com',
+    messagingSenderId: '334247834232',
+    appId: '1:334247834232:web:866b43440fb8a2b3b7f38d',
+    measurementId: 'G-CJ9E9PK1Z2',
 }
 
-const { BASE_API } = process.env
+// const { BASE_API } = process.env
+
+const BASE_API =
+    'https://innoday-developer-edition.eu44.force.com/services/apexrest'
 
 const APIS = {
     STATS: `${BASE_API}/getByDay`,
